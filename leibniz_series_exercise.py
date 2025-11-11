79ff1c9d4bcbc1e73c6453c81a6638da5a6a6bfa
ayahl5912-gif <ayahl5912@gmail.com>
2025-11-11 15:16:49 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,7 +1,8 @@
 def approximate_pi(n_terms):
-  apx_pi = 0
-  for i in range(n_terms):
-     apx_pi += ((-1)**i)/(2*i +1)*4
+    pi = 0
+    for i in range(n_terms):
+        pi += ((-1) ** i) / (2 * i + 1) * 4
 
-  return apx_pi
+    return pi
+
 
